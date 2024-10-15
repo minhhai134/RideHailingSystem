9e37f12c6537f8f46d325cc2de201e21790d496d
Hai Dam <minhhaizdr@gmail.com>
2024-10-15 08:31:32 +0700

    Initial commit

---
+++ src/main/java/EP/RideHailingSystem/RideHailingSystemApplication.java
@@ -0,0 +1,13 @@
+package EP.RideHailingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideHailingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideHailingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EP/RideHailingSystem/RideHailingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package EP.RideHailingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideHailingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
